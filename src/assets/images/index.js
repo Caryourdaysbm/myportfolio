2e27e2b4c75877cae1bac4ab60b99b11e45466b2
unknown <adeyemioluwakayode73@gmail.com>
2024-04-21 22:12:39 +0100

    my portfolio

---
+++ src/assets/images/index.js
@@ -0,0 +1,23 @@
+import hero from './hero.jpg'
+import meta from './meta.png'
+import shopify from './shopify.png'
+import starbucks from './starbucks.png'
+import tesla from './tesla.png'
+import upwork from './upwork-icon.svg'
+import Fiverr from './fiverr-icon.svg'
+import Zidio from './zidio_logo.png'
+import logo from './logo.svg'
+import Lantop from './Lantop.svg'
+
+export {
+    hero,
+    meta,
+    shopify,
+    starbucks,
+    tesla,
+    logo,
+    upwork,
+    Fiverr,
+    Zidio,
+    Lantop
+}
---
+++ src/constants/index.js
@@ -0,0 +1,247 @@
+import { meta, shopify, starbucks, Zidio, Fiverr, upwork, Lantop } from "../assets/images";
+import {
+    car,
+    css,
+    estate,
+    express,
+    git,
+    html,
+    javascript,
+    mongodb,
+    motion,
+    mui,
+    nodejs,
+    pricewise,
+    react,
+    redux,
+    sass,
+    snapgram,
+    summiz,
+    tailwindcss,
+    threads,
+} from "../assets/icons";
+import python from '../assets/images/python_logo.png'
+import django from '../assets/images/django_logo.png'
+import flask from '../assets/images/flask_logo.png'
+import Postgresql from '../assets/images/postgresql_logo.png'
+import x_logo from '../assets/icons/x-logo.svg'
+import LinkedIn from '../assets/icons/linkedin-logo.svg'
+import github from '../assets/icons/github-logo.svg'
+import contact from '../assets/icons/contact.svg'
+
+
+export const skills = [
+    {
+        imageUrl: css,
+        name: "CSS",
+        type: "Frontend",
+    },
+    {
+        imageUrl: express,
+        name: "Express",
+        type: "Backend",
+    },
+    {
+        imageUrl: git,
+        name: "Git",
+        type: "Version Control",
+    },
+    {
+        imageUrl: github,
+        name: "GitHub",
+        type: "Version Control",
+    },
+    {
+        imageUrl: html,
+        name: "HTML",
+        type: "Frontend",
+    },
+    {
+        imageUrl: javascript,
+        name: "JavaScript",
+        type: "Frontend",
+    },
+    {
+        imageUrl: mongodb,
+        name: "MongoDB",
+        type: "Database",
+    },
+    {
+        imageUrl: motion,
+        name: "Motion",
+        type: "Animation",
+    },
+    {
+        imageUrl: mui,
+        name: "Material-UI",
+        type: "Frontend",
+    },
+    {
+        imageUrl: python,
+        name: "Python",
+        type: "Language",
+    },
+    {
+        imageUrl: nodejs,
+        name: "Node.js",
+        type: "Backend",
+    },
+    {
+        imageUrl: react,
+        name: "React",
+        type: "Frontend",
+    },
+    {
+        imageUrl: redux,
+        name: "Redux",
+        type: "State Management",
+    },
+    {
+        imageUrl: sass,
+        name: "Sass",
+        type: "Frontend",
+    },
+    {
+        imageUrl: tailwindcss,
+        name: "Tailwind CSS",
+        type: "Frontend",
+    },
+    {
+        imageUrl: django,
+        name: "django",
+        type: "backend",
+    },
+    {
+        imageUrl: flask,
+        name: "flask",
+        type: "backend",
+    },
+    {
+        imageUrl: Postgresql,
+        name: "Postgresql",
+        type: "backend",
+    }
+];
+
+export const experiences = [
+    {
+        title: "Software Engineer, Frontend",
+        company_name: "CDLP Hub LTD",
+        icon: upwork,
+        iconBg: "#385925",
+        date: "October 2023 - April 2024",
+        points: [
+            "Developing and maintaining web applications using React.js and other related technologies.",
+            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+            "Implementing responsive design and ensuring cross-browser compatibility.",
+            "Participating in code reviews and providing constructive feedback to other developers.",
+        ],
+    },
+    {
+        title: "Full stack Developer",
+        company_name: "Fiverr",
+        icon: Fiverr,
+        iconBg: "#00b22d",
+        date: "Jan 2021 - Feb 2022",
+        points: [
+            "Developing and maintaining web applications using React.js and other related technologies.",
+            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+            "Implementing responsive design and ensuring cross-browser compatibility.",
+            "Participating in code reviews and providing constructive feedback to other developers.",
+        ],
+    },
+    {
+        title: "Full stack Developer",
+        company_name: "Zidio Development",
+        icon: Zidio,
+        iconBg: "#c51bf0",
+        date: "Jan 2022 - Jan 2023",
+        points: [
+            "Developing and maintaining web applications using React.js and other related technologies.",
+            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+            "Implementing responsive design and ensuring cross-browser compatibility.",
+            "Participating in code reviews and providing constructive feedback to other developers.",
+        ],
+    },
+    {
+        title: "Web Developer Intern",
+        company_name: "Lantop Infotech",
+        icon: Lantop,
+        iconBg: "#fff",
+        date: "Aug 2021 - Jan 2022",
+        points: [
+            "Developing and maintaining web applications using React.js and other related technologies.",
+            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+            "Implementing responsive design and ensuring cross-browser compatibility.",
+            "Participating in code reviews and providing constructive feedback to other developers.",
+        ],
+    },
+];
+
+export const socialLinks = [
+    {
+        name: 'contact',
+        iconUrl: contact,
+        link: '/contact',
+    },
+    {
+        name: 'GitHub',
+        iconUrl: github,
+        link: 'https://github.com/Caryourdaysbm',
+    },
+    {
+        name: 'LinkedIn',
+        iconUrl: LinkedIn,
+        link: 'https://www.linkedin.com/in/adeyemioluwakayode',
+    },
+    {
+        name: 'X-logo',
+        iconUrl: x_logo,
+        link: 'https://www.twitter.com/caryourday_sbm',
+    }
+];
+
+export const projects = [
+    {
+        iconUrl: pricewise,
+        theme: 'btn-back-red',
+        name: 'Mindmaze',
+        description: 'Developed a web application that tracks and notifies users of price changes for products on Amazon, helping users find the best deals.',
+        link: 'https://github.com/adrianhajdin/pricewise',
+    },
+    {
+        iconUrl: threads,
+        theme: 'btn-back-green',
+        name: 'Wander watch',
+        description: 'Created a full-stack replica of the popular discussion platform "Threads," enabling users to post and engage in threaded conversations.',
+        link: 'https://github.com/adrianhajdin/threads',
+    },
+    {
+        iconUrl: car,
+        theme: 'btn-back-blue',
+        name: 'Sommarizer',
+        description: 'Designed and built a mobile app for finding and comparing cars on the market, streamlining the car-buying process.',
+        link: 'https://github.com/adrianhajdin/project_next13_car_showcase',
+    },
+    {
+        iconUrl: snapgram,
+        theme: 'btn-back-pink',
+        name: 'Full Stack Instagram Clone',
+        description: 'Built a complete clone of Instagram, allowing users to share photos and connect with friends in a familiar social media environment.',
+        link: 'https://github.com/adrianhajdin/social_media_app',
+    },
+    {
+        iconUrl: estate,
+        theme: 'btn-back-black',
+        name: 'Real-Estate Application',
+        description: 'Developed a web application for real estate listings, facilitating property searches and connecting buyers with sellers.',
+        link: 'https://github.com/adrianhajdin/projects_realestate',
+    },
+    {
+        iconUrl: summiz,
+        theme: 'btn-back-yellow',
+        name: 'AI Summarizer Application',
+        description: 'App that leverages AI to automatically generate concise & informative summaries from lengthy text content, or blogs.',
+        link: 'https://github.com/adrianhajdin/project_ai_summarizer',
+    }
+];
